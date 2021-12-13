aa085ab5faa4c670bd86970f1b39e7ca01c335f4
Dustin Phillips <dudoop@comcast.net>
2021-12-13 15:56:45 -0600

    initial commit

---
+++ floats.go
@@ -0,0 +1,34 @@
+// Package datafile allows for reading data samples from files
+package datafile
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// GetFloats reads floating point numbers from files
+func GetFloats(fileName string) ([3]float64, error) {
+	var numbers [3]float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return numbers, err
+	}
+	scanner := bufio.NewScanner(file)
+	i := 0
+	for scanner.Scan() {
+		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		i++
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+	return numbers, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fiveforever55/datafile
+
+go 1.17
